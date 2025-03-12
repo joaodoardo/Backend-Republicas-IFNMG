f5c5bb8fefb94cdacbf8590dd3352337cdd9d9a4
João Eduardo <joaojaiba2023@gmail.com>
2025-03-12 03:22:45 -0300

    primeiro commit

---
+++ backend/index.js
@@ -0,0 +1,65 @@
+require("dotenv").config();
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const app = express();
+const prisma = new PrismaClient();
+const SECRET_KEY = process.env.SECRET_KEY;
+app.use(bodyParser.json());
+
+//registro
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await prisma.user.create({
+      data: { name, email, password: hashedPassword },
+    });
+    res.status(201).json({ message: "Usuário registrado com sucesso!" });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+//login
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ error: "Credenciais inválidas" });
+    }
+    const token = jwt.sign({ userId: user.id }, SECRET_KEY, { expiresIn: "30m" });
+    res.json({ token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+//autenticação
+const authenticate = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ error: "Acesso negado" });
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY);
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "Token inválido" });
+  }
+};
+
+//listar usuários (somente autenticados)
+app.get("/users", authenticate, async (req, res) => {
+  const users = await prisma.user.findMany({
+    select: { name: true, email: true },
+  });
+  res.json(users);
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`rodando em http://localhost:${PORT}`);
+});
